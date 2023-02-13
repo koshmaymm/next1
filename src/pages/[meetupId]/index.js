2bfb88b8ef75ce3e47519e4dd0a478fcf0352088
Mikhail <Mykhailo.Koshmai@dataart.com>
2023-02-13 16:03:50 +0200

    added component

---
+++ src/pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "@/app/components/meetups/MeetupDetail";
+
+function MeetupDetailsPage() {
+  return (
+    <MeetupDetail
+      img="https://cdn.pixabay.com/photo/2018/04/16/16/16/sunset-3325080__340.jpg"
+      title="Some title"
+      address="Some Adress"
+      description="This is desctiption 1"
+    />
+  );
+}
+
+export default MeetupDetailsPage;
