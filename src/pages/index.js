9de75d0abe1f007697b7ef5e874eda6cde19328b
Mikhail <Mykhailo.Koshmai@dataart.com>
2023-02-13 14:12:14 +0200

    added components, styles, wrapper

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,31 @@
+import MeetupList from "@/app/components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014__340.jpg",
+    description: "This is a description",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823__340.jpg",
+    description: "This is a description",
+  },
+  {
+    id: "m3",
+    title: "A Third Meetup",
+    image:
+      "https://cdn.pixabay.com/photo/2012/03/01/00/55/flowers-19830__340.jpg",
+    description: "This is a description",
+  },
+];
+
 const HomePage = () => {
-    return <h1>HomePage</h1>;
-}
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+};
 
 export default HomePage;
